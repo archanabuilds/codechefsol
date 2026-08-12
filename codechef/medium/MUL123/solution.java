3299ab24c2dc85981c458aed69126ba17b8fa5b7
archanabuilds <archana03964@gmail.com>
2026-08-12 20:22:29 +0530

    ✅ MUL123 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

---
+++ codechef/medium/MUL123/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+          Scanner sc = new Scanner(System.in);
+          int t = sc.nextInt();
+          while(t-- > 0)
+          {
+              int count = 0;
+              int n = sc.nextInt();
+              if( n % 3 == 0)
+              System.out.println(0);
+              while( n % 3 == 0)
+              {
+                  
+              }
+          }
+	}
+}
