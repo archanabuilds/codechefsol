f88dd426880b33453b47b6a0a27728fce19fef6e
archanabuilds <archana03964@gmail.com>
2026-08-19 21:16:43 +0530

    ✅ MRSWAP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/MRSWAP/solution.java
+++ codechef/medium/MRSWAP/solution.java
@@ -7,6 +7,23 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n = sc.nextInt();
+		    int[] a = new int[2*n];
+		    for(int i = 0; i < 2 * n; i++)
+		     a[i] = sc.nextInt();
+		     
+		    int sum = 0;
+		    
+		    for(int i = 0; i < n; i++)
+		    {
+		        sum += Math.max(a[i],a[2*n-1-i]);
+		    }
+		    System.out.println(sum);
+		}
 
 	}
 }
