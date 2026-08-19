d6f0476f11e656a1895684d44a6dff729017e059
archanabuilds <archana03964@gmail.com>
2026-08-19 21:20:40 +0530

    ✅ ODDEVEN7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

--- codechef/medium/ODDEVEN7/solution.java
+++ codechef/medium/ODDEVEN7/solution.java
@@ -7,6 +7,26 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+	    {
+	        int n = sc.nextInt();
+	        int o = 0, e = 0;
+	        for(int i = 0; i < n; i++)
+	        {
+	            int x = sc.nextInt();
+	            if(x % 2 == 0)
+	                e++;
+	            else
+	               o++;
+	        }
+	        int ans = 2 * Math.min(o,e);
+	        if(o != e)
+	          ans++;
+	          
+	          System.out.println(ans);
+	    }
 
 	}
 }
