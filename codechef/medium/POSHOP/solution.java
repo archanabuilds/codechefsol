b227c28b57919b2567539fac253fa1689b8dc72e
archanabuilds <archana03964@gmail.com>
2026-08-26 20:19:08 +0530

    ✅ POSHOP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-26

--- codechef/medium/POSHOP/solution.java
+++ codechef/medium/POSHOP/solution.java
@@ -11,7 +11,25 @@ class Codechef
        int t = sc.nextInt();
        while(t-- > 0)
        {
-           
+           int n = sc.nextInt();
+           int[] c = new int[n];
+           for(int i = 0; i < n; i++)
+            c[i] = sc.nextInt();
+            
+            int ans = 0;
+            for(int i = 0; i < n; i++)
+              ans = Math.max(c[i],ans);
+            for(int i = 0 ; i < n; i++)
+             {
+                 for(int j = i + 1; j < n;j++)
+                 {
+                     if(c[i] <= c[j])
+                     {
+                         ans = Math.max(ans,c[i] + c[j]);
+                     }
+                 }
+             }  
+             System.out.println(ans);
        }
 	}
 }
