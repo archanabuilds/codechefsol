5a91dbde091a7814b4dd5a04587290034cabe90c
archanabuilds <archana03964@gmail.com>
2026-09-02 20:51:53 +0530

    ✅ THREEGAME | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/THREEGAME/solution.java
+++ codechef/medium/THREEGAME/solution.java
@@ -15,7 +15,7 @@ class Codechef
             if(n == 1)
              System.out.println(1);
             else
-            System.out.println(n + 2);
+            System.out.println(4);
         }
 	}
 }
