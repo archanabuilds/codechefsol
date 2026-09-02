c7c84a1be576c3e11c26f3308c009449bfeaa57d
archanabuilds <archana03964@gmail.com>
2026-09-02 20:27:11 +0530

    ✅ MISSINGNUM7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/MISSINGNUM7/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		System.out.println(10 - s);
+
+	}
+}
