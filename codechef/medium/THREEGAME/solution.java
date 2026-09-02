842758131fb313299bdddfdeede0ad1e5194426f
archanabuilds <archana03964@gmail.com>
2026-09-02 20:42:12 +0530

    ✅ THREEGAME | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/medium/THREEGAME/solution.java
+++ codechef/medium/THREEGAME/solution.java
@@ -7,6 +7,15 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+            if(n == 1)
+             System.out.println(1);
+            else
+            System.out.println(n + 2);
+        }
 	}
 }
