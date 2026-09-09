117c40d8cd03332d770c7748c0d368a3300104f0
archanabuilds <archana03964@gmail.com>
2026-09-09 20:34:31 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.java
+++ codechef/medium/UNQSHF/solution.java
@@ -7,6 +7,33 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n = sc.nextInt();
+		    String a = sc.next();
+		    String b = sc.next();
+		    
+		    int counta = 0,countb = 0;
+		    
+		    for(int i = 0; i < n;i++)
+		    {
+		        if(a.charAt(i) == 'a')
+		        {
+		            counta++;
+		        }
+		        if(b.charAt(i) == 'a')
+		        {
+		            countb++;
+		        }
+		    }
+		    if(counta + countb == n)
+		      System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+		
 
 	}
 }
