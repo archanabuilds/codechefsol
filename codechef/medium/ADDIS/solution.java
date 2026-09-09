6e466b2bddcdaf0919934d6541a39e95b5e46b8b
archanabuilds <archana03964@gmail.com>
2026-09-09 20:38:58 +0530

    ✅ ADDIS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/ADDIS/solution.java
+++ codechef/medium/ADDIS/solution.java
@@ -7,6 +7,27 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int  n = sc.nextInt();
+		    
+		    HashMap<Integer,Integer> map = new HashMap<>();
+		    int max = 0;
+		    
+		    for(int i = 0; i < n; i++)
+		    {
+		        int x = sc.nextInt();
+		        
+		        int freq = map.getOrDefault(x,0) + 1;
+		        map.put(x,freq);
+		        
+		        max = Math.max(max,freq);
+		        
+		    }
+		    System.out.println((max + 1)/2);
+		}
 
 	}
 }
