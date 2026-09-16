85df63dab9e0bc4fb9bebdd3bc339a1a9c326049
archanabuilds <archana03964@gmail.com>
2026-09-16 20:46:32 +0530

    ✅ FIXEDPTS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/FIXEDPTS/solution.java
+++ codechef/medium/FIXEDPTS/solution.java
@@ -7,6 +7,20 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    if(n - k == 0)
+		    {
+		        System.out.println("No");
+		    }
+		    else{
+		        System.out.println("Yes");
+		    }
+		}
 
 	}
 }
