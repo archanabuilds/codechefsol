64d82ee984b121baf016d7a4687e780250769b3a
archanabuilds <archana03964@gmail.com>
2026-09-16 20:41:00 +0530

    ✅ SHARING | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/SHARING/solution.java
+++ codechef/medium/SHARING/solution.java
@@ -8,7 +8,16 @@ class Codechef
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		
+		if((a - b) % 2 == 0)
+		{
+		    System.out.println((a - b)/ 2);
+		}
+		else{
+		    System.out.println(-1);
+		}
 
 	}
 }
