98d4f05489b071290d1abc5cdab7ae6bf5600b54
archanabuilds <archana03964@gmail.com>
2026-09-16 20:49:44 +0530

    ✅ FIXEDPTS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/FIXEDPTS/solution.java
+++ codechef/medium/FIXEDPTS/solution.java
@@ -13,12 +13,12 @@ class Codechef
 		{
 		    int n = sc.nextInt();
 		    int k = sc.nextInt();
-		    if(k == n || k <= n - 2)
+		    if(n - k == 0)
 		    {
-		        System.out.println("Yes");
+		        System.out.println("No");
 		    }
 		    else{
-		        System.out.println("No");
+		        System.out.println("Yes");
 		    }
 		}
 
