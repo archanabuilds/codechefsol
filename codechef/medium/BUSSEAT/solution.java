dfcde9ceca04a75d17c2e674f2086b8d85a19abd
archanabuilds <archana03964@gmail.com>
2026-09-16 20:43:22 +0530

    ✅ BUSSEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.java
+++ codechef/medium/BUSSEAT/solution.java
@@ -7,6 +7,21 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    if(k <= n)
+		    {
+		        System.out.println(0);
+		    }
+		    else{
+		        System.out.println(2 *(k - n));
+		    }
+		}
+		
 
 	}
 }
