8b45c0e65f18ba9f6577e7acc3a46193c627592c
archanabuilds <archana03964@gmail.com>
2026-09-23 20:34:51 +0530

    ✅ FALLPR | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/FALLPR/solution.java
+++ codechef/medium/FALLPR/solution.java
@@ -7,6 +7,41 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    
+		    int n = sc.nextInt();
+		    int[] a = new int[n];
+		    for(int i = 0; i < n; i++)
+		    {
+		        a[i] = sc.nextInt();
+		    }
+		    
+		    long sum = 0;
+		    int min = Integer.MAX_VALUE;
+		    boolean deleted = false;
+		    boolean possible = true;
+		    
+		    for(int i = 0; i< n; i++)
+		    {
+		        sum += a[i];
+		        min = Math.min(min,a[i]);
+		        if(sum < 0)
+		        {
+		            if(deleted)
+		            {
+		                possible = false;
+		                break;
+		            }
+		            sum -= min;
+		            deleted = true;
+		        }
+		    }
+		    System.out.println(possible ? "YES" : "NO");
+		}
+		sc.close();
 
 	}
 }
