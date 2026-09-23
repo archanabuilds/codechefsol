3693bc33ac733f3ff8a399f65551cd5cb2bd8dc6
archanabuilds <archana03964@gmail.com>
2026-09-23 20:25:01 +0530

    ✅ ECHOECHO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/ECHOECHO/solution.java
+++ codechef/medium/ECHOECHO/solution.java
@@ -9,7 +9,14 @@ class Codechef
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		
+		if(s.charAt(0) == s.charAt(2) && s.charAt(1) == s.charAt(3))
+		{
+		    System.out.println("Yes");
+		}
+		else
+		{
+		    System.out.println("No");
+		}
 
 	}
 }
