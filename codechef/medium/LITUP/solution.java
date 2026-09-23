069236d2792801d8b9fe1334f952cf77b1963d5e
archanabuilds <archana03964@gmail.com>
2026-09-23 20:40:49 +0530

    ✅ LITUP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/LITUP/solution.java
+++ codechef/medium/LITUP/solution.java
@@ -7,6 +7,34 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    int[] c = new int[n];
+		    for(int i = 0; i < n; i++)
+		    {
+		        c[i] = sc.nextInt();
+		    }
+		    int ans = Integer.MAX_VALUE;
+		    for(int i = 0; i < n; i++)
+		    {
+		        for(int j = 0; j < n; j++)
+		        {
+		            if(i-k <= 0 && j+k >= n-1 && j-1 <= 2*k)
+		            {
+		                ans = Math.min(ans,c[i]+c[j]);
+		            }
+		        }
+		    }
+		    if(ans == Integer.MAX_VALUE)
+		    System.out.println(-1);
+		    else
+		    System.out.println(ans);
+		}
+		
 
 	}
 }
